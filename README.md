580e9026b03594e10d3e5363fd0ccf72218743bc
Jonas Wolfram <jonas-wolfram@web.de>
2020-05-03 18:19:14 +0200

    Adjustements to slides

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Todos left: 
 
-- Image-Upload should not be done with one single JSON request and with the image being Base64-encoded. It should be done doing to requests. First one should only store the image and return the path. The second one should create the resource and store the path on one of the propertie's values.
 - Header and Filters-Sidebar should be two different components for mobile and desktop Layout. Depending on the user device either one or the other component should be rendered. Therefore complex CSS media queries can be omitted.
 - The infinite slideshow has been made by inserting a style tag with dynamically generated keyframes. This is an anti-pattern for React.
 - The CustomLink in each slide leads to a path defined while inserting. Each slide should reference an existing project document. (n:1)
--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -11,43 +11,51 @@ import styles from './Slideshow.module.scss';
 const Slideshow = ({ slides }) => {
     const [loadedImagesCounter, setLoadedImagesCounter] = useState(0);
 
-    const slideFrozenTimeInSeconds = 5;
-    const slideTransitionTimeInSeconds = 0.5;
-    const animationDuration = slides.length * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds);
-    const oneSecondInPercent = 100 / animationDuration;
-
-    let keyframes;
-    if (slides.length >= 3) {
-        keyframes = `@keyframes fade-out {
-            0% { opacity: 1; z-index: 2; }
-            ${oneSecondInPercent * slideFrozenTimeInSeconds}% { opacity: 1; z-index: 2; }
-            ${oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds)}% { opacity: 0; z-index: 2; }
-            ${oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds)}.01% { opacity: 0; z-index: 0; }
-            ${100 - oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds) - 0.01}% { opacity: 0; z-index: 0; }
-            ${100 - oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds)}% { opacity: 1; z-index: 1; }
-            100% { opacity: 1; z-index: 1; }
-        }`;
-    }
-
-    return (
-        <div className={`${styles.slideshow} ${slides.length > 0 && loadedImagesCounter === slides.length ? styles.visible : ""}`}>
-            {loadedImagesCounter !== slides.length &&
+    // Less than three slides leads to bugs
+    if (slides.length <= 2) {
+        console.warn("Not enough slides provided for the slideshow to start");
+        return (
+            <div className={styles.slideshow}>
                 <div className={styles.spinnerContainer}>
                     <Spinner colorMode="dark" />
                 </div>
-            }
-            {keyframes &&
+            </div>
+        )
+    }
+    else {
+        const slideFrozenTimeInSeconds = 5;
+        const slideTransitionTimeInSeconds = 0.5;
+        const animationDuration = slides.length * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds);
+        const oneSecondInPercent = 100 / animationDuration;
+        const haveAllImagesBeenLoaded = loadedImagesCounter === slides.length;
+
+        const keyframes = `@keyframes fade-out {
+            0% { opacity: 1; z-index: 2; pointer-events: all; }
+            ${oneSecondInPercent * slideFrozenTimeInSeconds}% { opacity: 1; z-index: 2; pointer-events: all; }
+            ${oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds)}% { opacity: 0; z-index: 2; pointer-events: none; }
+            ${oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds)}.01% { opacity: 0; z-index: 0; pointer-events: none; }
+            ${100 - oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds) - 0.01}% { opacity: 0; z-index: 0; pointer-events: none; }
+            ${100 - oneSecondInPercent * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds)}% { opacity: 1; z-index: 1; pointer-events: none; }
+            100% { opacity: 1; z-index: 1; pointer-events: none; }
+        }`;
+
+        return (
+            <div className={`${styles.slideshow} ${haveAllImagesBeenLoaded ? styles.visible : ""}`}>
                 <style>
                     {keyframes}
                 </style>
-            }
-            {
-                slides.map((slide, slideIndex) => {
+                {!haveAllImagesBeenLoaded &&
+                    <div className={styles.spinnerContainer}>
+                        <Spinner colorMode="dark" />
+                    </div>
+                }
+                {slides.map((slide, slideIndex) => {
                     const { title, subtitle, projectName, image, buttonColor, colorMode } = slide;
                     const style = {
                         animationName: "fade-out",
                         animationDuration: animationDuration + "s",
                         animationDelay: (slides.length - 1 - slideIndex) * (slideFrozenTimeInSeconds + slideTransitionTimeInSeconds) + "s",
+                        animationPlayState: haveAllImagesBeenLoaded ? "running" : "paused"
                     }
                     return (
                         <div key={slideIndex} className={styles.slideContainer} style={style}>
@@ -63,9 +71,10 @@ const Slideshow = ({ slides }) => {
                         </div>
                     )
                 })
-            }
-        </div>
-    )
+                }
+            </div>
+        )
+    }
 }
 
 Slideshow.propTypes = {
